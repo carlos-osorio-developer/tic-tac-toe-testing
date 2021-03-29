bf2c1cff35bbbf76ca1ba50febe601c7e6acbaaa
Amalcxc <amalcxirsi@gmail.com>
2021-03-29 23:54:23 +0300

    change the tic tac teo code

--- bin/main.rb
+++ bin/main.rb
@@ -1,90 +1,88 @@
+#!/usr/bin/env ruby
+
 require_relative '../lib/game_logic'
 
-module UserInteraction
-  def user_move(user1, user2, board1)
-    player_turn = board1.moves_counter.odd? ? user1 : user2
-    puts "#{player_turn.name} please enter a move:"
-    move_p = gets.chomp.to_i
-    board1.make_a_move(move_p, player_turn.marker)
-    board1.print_board
-    board1.plays_array
-  end
+player1 = Player.new('X')
+player2 = Player.new('O')
 
-  def print_winner(winning_marker, user1, user2)
-    if user1.marker == winning_marker
-      puts "#{user1.name} Wins!"
-    else
-      puts "#{user2.name} Wins!"
-    end
-  end
+while player1.name == ''
+  puts 'enter player one name?'
+  player1.name = gets.chomp
 end
 
-# Player Class: Initialize player name and marker
-class Player
-  attr_reader :name, :marker
-
-  def initialize(name, marker)
-    @name = name
-    @marker = marker
-  end
+while player2.name == ''
+  puts 'enter player two name?'
+  player2 = Player.new('O')
+  player2.name = gets.chomp
 end
 
-# Board Class: Draws the board with the current play, checks valid moves, counts moves
-class Board
-  attr_accessor :plays_array
-  attr_reader :moves_counter
+board = Board.new
 
-  def initialize(plays_array)
-    @plays_array = plays_array
-    @moves_counter = 1
-  end
+system 'clear'
+system 'cls'
 
-  def print_board
-    col_separator = ' | '
-    row_separator = ' +---+---+---+'
-    puts row_separator
-    3.times do |rows|
-      print col_separator
-      3.times { |columns| print (@plays_array[columns + (rows * 3)]).to_s + col_separator }
-      puts "\n#{row_separator}"
-    end
-  end
+puts board.create_board
 
-  def make_a_move(num, marker)
-    if !num.is_a?(Integer) || !num.between?(1, 9)
-      puts 'Invalid move. Please enter a number between 1 and 9'
-      return false
-    elsif @plays_array[num - 1].is_a?(String)
-      return puts 'Invalid move. Position already taken'
-    else
-      @plays_array[num - 1] = marker
-      @moves_counter += 1
-    end
+puts "#{player1.name} will be X"
+
+puts "#{player2.name} will be O"
+
+puts player1.letter.to_s
+
+sleep 1
+
+while board.game
+  system 'clear'
+  system 'cls'
+  until board.valid
+    puts board.create_board
+    puts "It's #{player1.name} turn"
+    puts ''
+    input = gets.chomp.to_i
+    next unless board.check_valid_move(input) == false
+
+    system 'cls'
+    puts 'Please use a valid number'
+    sleep 1
   end
-  include UserInteraction
-  include GameLogic
+  board.square[input - 1] = player1.letter
+  system 'cls'
+  system 'clear'
+  puts board.create_board
+  if board.win(player1)
+    puts "#{player1.name} is the WINNER"
+    break
+  end
+  if board.draw
+    puts "It's a draw!"
+    break
+  end
+  system 'clear'
+  system 'cls'
+  board.valid = false
+  until board.valid
+    system 'clear'
+    system 'cls'
+    puts board.create_board
+    puts "It's #{player2.name} turn"
+    puts ''
+    input = gets.chomp.to_i
+    next unless board.check_valid_move(input) == false
+
+    system 'cls'
+    puts 'Please use a valid number'
+    sleep 1
+  end
+  board.square[input - 1] = player2.letter
+  system 'cls'
+  system 'clear'
+  puts board.create_board
+  if board.win(player2)
+    puts "#{player2.name} is the WINNER"
+    break
+  end
+  board.valid = false
+  system 'clear'
+  system 'cls'
+  puts board.create_board
 end
-
-# playername = ''
-# while playername.empty?
-#   puts 'Enter player 1 name'
-#   playername = gets.chomp
-#   player1 = Player.new(playername, 'X')
-# end
-
-# playername = ''
-# while playername.empty?
-#   puts 'Enter player 2 name'
-#   playername = gets.chomp
-#   player2 = Player.new(playername, 'O')
-# end
-
-# board1 = Board.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
-# board1.print_board
-
-# while board1.moves_counter <= 9
-#   updated_board = board1.user_move(player1, player2, board1)
-#   winning_marker = board1.check_winner(updated_board)
-#   board1.print_winner(winning_marker, player1, player2) if winning_marker
-#   puts "It's a Draw" if board1.moves_counter == 10
-# end
--- spec/testing_spec.rb
+++ spec/testing_spec.rb
@@ -3,7 +3,7 @@ require_relative '../lib/game_logic.rb'
 
 describe Board do
     
-    describe "#make_a_move" do      
+    describe "check_valid_move" do      
       it "replace a number with the marker for a valid move" do        
         board1 = Board.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
         expect(board1.make_a_move("X", "X")).to eql(2)
