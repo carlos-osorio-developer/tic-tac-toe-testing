de5d1b9cc9faaec84c5808168413d9f71c910e8c
Amalcxc <amalcxirsi@gmail.com>
2021-03-30 00:42:19 +0300

    add the winner test for players

--- spec/testing_spec.rb
+++ spec/testing_spec.rb
@@ -11,6 +11,17 @@ describe Board do
       it "invalid move do not returns true" do        
         board1 = Board.new
         expect(board1.check_valid_move(55)).not_to eql(true)
+      end 
+    end
+
+    describe "#win" do
+      it "winner player returns true" do
+        board = Board.new
+        player1 = Player.new("x")
+        board.square[0] = player1.letter
+        board.square[1] = player1.letter
+        board.square[2] = player1.letter
+        expect(board.win(player1)).to eql(true) 
       end
     end
   end
